cfbe1f00821ec2a7cd5a3c6f48041df726cb417d
Jeffrey <jeffrey.graham600@gmail.com>
2022-07-10 19:02:08 -0400

    changed datastructure to hold more values in die

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,25 @@ function App() {
   const randomTenNumbers = () => {
     let result = [];
     for (let i = 0; i < 10; i++) {
-      result.push(Math.ceil(Math.random() * 6));
+      result.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: i,
+      });
     }
     return result; //creating an array of 10 random numbers
   };
 
   const [allDie, allDieState] = useState(randomTenNumbers());
+  
 
   const reroll = () => {
     allDieState((prevState) => (prevState = randomTenNumbers()));
   };
 
-  let diceElements = allDie.map((dice) => <Die value={dice} />);
+  let diceElements = allDie.map((dice) => (
+    <Die value={dice.value} id={dice.id} isHeld={dice.isHeld} />
+  ));
 
   return (
     <main>
