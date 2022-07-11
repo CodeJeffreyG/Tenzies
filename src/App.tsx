7bf4dd079c841d0219716be2eae6b75d7d8c3da8
Jeffrey <jeffrey.graham600@gmail.com>
2022-07-10 23:55:43 -0400

    Set WinCon with useEffecthook styled title for win

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Die from "./Components/Die";
 import Button from "./Components/Button";
+import { count } from "console";
 
 function App() {
   const randomTenNumbers = () => {
@@ -15,7 +16,33 @@ function App() {
     return result; //creating an array of 10 random numbers
   };
 
+  //array of random numbers 1 - 6 state
   const [allDie, allDieState] = useState(randomTenNumbers());
+
+  //win con state
+  const [tenzies, setTenzies] = useState(false);
+
+  //counter state
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    //is the "isHeld" property true for every die?
+    const areHeld = allDie.every((allDie) => allDie.isHeld);
+    //reference to check if all "value properties are the same"
+    const firstValue = allDie[0].value;
+    // is the "value" property the same number?
+    const allValueSame = allDie.every((allDie) => firstValue === allDie.value);
+
+    //if so change Tenzie State
+    if (areHeld && allValueSame) {
+      setTenzies((prevValue) => !prevValue);
+    }
+  }, [allDie]);
+
+  //increment counter
+  useEffect(() => {
+    setCounter((prevCounter) => prevCounter + 1);
+  }, [allDie]);
 
   const reroll = () => {
     allDieState((prevState) =>
@@ -49,13 +76,13 @@ function App() {
 
   return (
     <main>
-      <h1 className="title">Tenzies</h1>
+      <h1 className="title">{tenzies ? "You Won!" : "Tenzies"}</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
       <div className="diceContainer">{diceElements}</div>
-      <Button reroll={reroll} />
+      <Button reroll={reroll} win={tenzies} increment={counter} />
     </main>
   );
 }
